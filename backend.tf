a892b6eab07e35ff449610ed2610e8d5b5aa11f2
Marwan Ayman <30570547+maroayman@users.noreply.github.com>
2025-11-14 20:28:28 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket       = "git-bucket-s3-24-2-4-24"
     key          = "eks/terraform.tfstate"
-    region       = "eu-west-1"
+    region       = "us-east-1"
     encrypt      = true
     use_lockfile = true
   }
